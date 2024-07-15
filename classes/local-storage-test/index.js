7444089954ff1fe0efbb8c0dae741d7c83f0bc3f
akhilkailas017 <akhilkailas2001@gmail.com>
2024-07-15 10:11:24 +0530

    certiapp

--- classes/local-storage-test/index.js
+++ classes/local-storage-test/index.js
@@ -1,4 +1,6 @@
+// index.js
 
+// Function to store certificate data
 function storeCertificateData() {
     const course = document.querySelector('select[name="course"]').value;
     const certId = document.querySelector('input[name="name"]').value;
@@ -6,6 +8,7 @@ function storeCertificateData() {
     const grade = document.querySelector('select[name="grade"]').value;
     const issueDate = document.querySelector('input[name="issuedate"]').value;
 
+    // Create certificate object
     const certificate = {
         course,
         certId,
@@ -14,16 +17,17 @@ function storeCertificateData() {
         issueDate
     };
 
+    // Store certificate object in localStorage
     localStorage.setItem(certId, JSON.stringify(certificate));
 }
 
-
+// Function to retrieve certificate data
 function retrieveCertificateData(certId) {
     const certificate = localStorage.getItem(certId);
     return certificate ? JSON.parse(certificate) : null;
 }
 
-
+// Add event listener to the form on issueCertificate.html
 document.querySelector('form').addEventListener('submit', function (event) {
     event.preventDefault();
     storeCertificateData();
