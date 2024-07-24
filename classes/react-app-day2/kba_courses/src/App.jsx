bc578d4219ef46b1f65ea503d9e5b746a175dfec
akhilkailas017 <akhilkailas2001@gmail.com>
2024-07-24 14:19:56 +0530

    a

---
+++ classes/react-app-day2/kba_courses/src/App.jsx
@@ -0,0 +1,27 @@
+
+
+import Button from './components/Button'
+import CourseCards from './components/CourseCards'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+function App() {
+  
+
+  return (
+    <>
+    {/* <!-- navbar  --> */}
+    <Navbar/>
+
+   {/* <!-- hero  --> */}
+   <Hero/>
+
+   {/* <!-- cards --> */}
+    <CourseCards/>
+
+   {/* <!-- all courses button --> */}
+    <Button/>
+    </>
+  )
+}
+
+export default App
